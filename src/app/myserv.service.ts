2dfdcb27e9f03e0eb23923358d78b671cb255b64
tasoskak <tasoskakouris@gmail.com>
2018-04-21 21:58:45 +0300

    Update myserv.service.ts

--- src/app/myserv.service.ts
+++ src/app/myserv.service.ts
@@ -2,8 +2,6 @@ import { Injectable } from '@angular/core';
 import {BehaviorSubject} from 'rxjs/BehaviorSubject';
 @Injectable()
 export class MyservService {
-  // bima 1 dimourgo ena variable user 
-  // pou einai tha ine object behaviosubject
   private user = new BehaviorSubject<string>('john');
   cast = this.user.asObservable();
 
